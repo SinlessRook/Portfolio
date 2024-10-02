c0d5dfc57a1f19ca3339ae41b8fab509126b073a
Sinless Rook <142495840+SinlessRook@users.noreply.github.com>
2024-10-02 11:09:52 +0530

    imageSection

--- src/assets/Images/index.js
+++ src/assets/Images/index.js
@@ -1,10 +1,10 @@
-import Image1 from '../Images/background.svg'
-import Image2 from '../Images/myImage.png'
-import Image3 from '../Images/github.svg'
-import Image4 from '../Images/instgram.svg'
-import Image5 from '../Images/linkedin.svg'
-import Image6 from '../Images/circle1.svg'
-import Image7 from '../Images/circle2.svg'
+import Image1 from './background.svg'
+import Image2 from './myImage.png'
+import Image3 from './github.svg'
+import Image4 from './instgram.svg'
+import Image5 from './linkedin.svg'
+import Image6 from './circle1.svg'
+import Image7 from './circle2.svg'
 
 const BgImage = Image1
 const MyImage = Image2
